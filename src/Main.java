fe4276ab5ccada92d4b5561df0ead26afddc241c
Игорь Романов <till5357@gmail.com>
2022-02-18 20:03:45 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 2519; //Цена билета
+
+        System.out.println((price / 20) + " Бонусные милли");  // 20р=1миля
+
+          }
+}
